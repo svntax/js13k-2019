bc9f50c1e42365d23ae5a77b97558cbbbcc48cd3
svntax <endless.answers@gmail.com>
2019-09-11 00:53:51 -0400

    Add hookshot pulling mechanic, basic score tracking, and placeholder respawning.

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
+var currentScore = 0;
+var highScore = 0;
+
+function addPoints(amount){
+	currentScore += amount;
+	if(currentScore > highScore){
+		highScore = currentScore;
+	}
+	document.getElementById("score").setAttribute("text", {
+		value: "Score: " + currentScore + "\nHigh Score: " + highScore
+	});
+}
+
 AFRAME.registerComponent("hookshot", {	
 	schema: {
 		hand: {default: ""}
@@ -6,42 +19,69 @@ AFRAME.registerComponent("hookshot", {
 	init: function(){
 		var data = this.data;
 		var el = this.el;
-		
 		var config = {
 			hand: this.data.hand,
 			model: false
 		};
+		el.setAttribute("vive-controls", config);
+		el.setAttribute("oculus-touch-controls", config);
 		
-		el.setAttribute('vive-controls', config);
-		el.setAttribute('oculus-touch-controls', config);
+		el.shot = document.getElementById("projectile")
 		
 		el.addEventListener("buttondown", function(){
 			//Hook projectile, which is separate from the controller-tracked model
-			var shot = document.getElementById("projectile");
-			shot.setAttribute("visible", true);
-			shot.setAttribute("position", el.object3D.position);
-			shot.object3D.rotation.x = el.object3D.rotation.x;
-			shot.object3D.rotation.y = el.object3D.rotation.y;
-			shot.object3D.rotation.z = el.object3D.rotation.z;
+			var shot = el.shot;
 			
-			//Get the forward direction of this controller
-			//Note: y and z are flipped in this operation, so the forward vector is in the y-axis
-			var point = new THREE.Vector3(0, -1, 0);
-			el.object3D.localToWorld(point);
-			var globalDir = point.sub(el.object3D.position);
-			var resultVel = globalDir.multiplyScalar(5);
-			shot.setAttribute("velocity", {
-				vel: resultVel //TODO test schema change of velocity component
-			});
-			//Need to emit this event in order for the velocity to actually update
-			//otherwise the element's properties (vel) carry over from before
-			shot.emit("updateVelocity", {x: resultVel.x, y: resultVel.y, z: resultVel.z}, false);
-		});
-		
-		el.addEventListener("hit", function(e){
-			console.log(e);
+			if(shot.isExtended){
+				//TODO could be interesting mechanic, to redirect the hookshot
+				//if(!shot.isReturning){
+					shot.isReturning = true;
+					var p1 = shot.object3D.position;
+					var p2 = el.object3D.position;
+					shot.homePos = p2;
+					var globalDir = new THREE.Vector3();
+					globalDir.subVectors(p2, p1).normalize().multiplyScalar(5);
+					shot.emit("updateVelocity", {x: globalDir.x, y: globalDir.y, z: globalDir.z}, false);
+					shot.emit("updateGravity", {gravity: 0});
+				//}
+			}
+			else{
+				shot.isExtended = true;
+				shot.setAttribute("visible", true);
+				el.querySelector("#hook").setAttribute("visible", false);
+				shot.setAttribute("position", el.object3D.position);
+				shot.object3D.rotation.x = el.object3D.rotation.x;
+				shot.object3D.rotation.y = el.object3D.rotation.y;
+				shot.object3D.rotation.z = el.object3D.rotation.z;
+				
+				//Get the forward direction of this controller
+				//Note: y and z are flipped in this operation, so the forward vector is in the y-axis
+				var point = new THREE.Vector3(0, -1, 0);
+				el.object3D.localToWorld(point);
+				var globalDir = point.sub(el.object3D.position);
+				var resultVel = globalDir.multiplyScalar(6);
+				//Need to emit this event in order for the velocity to actually update
+				//otherwise the element's properties (vel) carry over from before
+				shot.emit("updateVelocity", {x: resultVel.x, y: resultVel.y, z: resultVel.z}, false);
+				shot.emit("updateGravity", {gravity: -5});
+			}
 		});
 	},
+	
+	tick: function(time, deltaTime){
+		var el = this.el;
+		if(el.shot.isReturning){
+			var pos = el.shot.object3D.position;
+			//If within 0.5m of the home position
+			if(pos.distanceTo(el.shot.homePos) <= 0.5){
+				el.shot.isReturning = false;
+				el.shot.isExtended = false;
+				el.shot.setAttribute("visible", false);
+				el.querySelector("#hook").setAttribute("visible", true);
+				el.shot.emit("updateVelocity", {x: 0, y: 0, z: 0}, false);
+			}
+		}
+	}
 });
 
 AFRAME.registerComponent("fish", {
@@ -51,13 +91,11 @@ AFRAME.registerComponent("fish", {
 	
 	init: function(){
 		var vertices = [
-			/*1, 1, 1, 	- 1, - 1, 1, 	- 1, 1, - 1, 	1, - 1, - 1,*/
 			-1, 0, 0,    1, 0, 0,    0, 1, 0,    0, -1, 0,
 			0, 0, 1,    0, 0, -1
 		];
 		//Order of vertices matters, needs to be counter-clockwise order when facing camera
 		var indices = [
-			/*2, 1, 0, 	0, 3, 2,	1, 3, 0,	2, 3, 1*/
 			0, 4, 2,    4, 1, 2,    4, 0, 3,   4, 3, 1,
 			5, 0, 2,    5, 2, 1,   0, 5, 3,    5, 1, 3,
 		];
@@ -81,9 +119,38 @@ AFRAME.registerComponent("hook-target", {
 	init: function(){
 		var data = this.data;
 		var el = this.el;
+		el.caught = false; //Whether it's caught by the hookshot or not
+		el.hookshot = document.getElementById("projectile");
 		el.addEventListener("hit", function(e){
-			console.log("target hit");
+			if(!el.caught){
+				el.caught = true;
+				el.emit("updateVelocity", {x: 0, y: 0, z: 0}, false);
+			}
 		});
+	},
+	
+	tick: function(time, deltaTime){
+		var el = this.el;
+		var data = this.data;
+		
+		if(el.caught){
+			el.object3D.position.x = el.hookshot.object3D.position.x;
+			el.object3D.position.y = el.hookshot.object3D.position.y;
+			el.object3D.position.z = el.hookshot.object3D.position.z;
+			
+			var pos = el.object3D.position;
+			//If within 1.7m of the origin
+			if(pos.length() < 1.7){
+				el.caught = false;
+				//Get points for catching this fish
+				addPoints(10); //TODO triggers twice because both hands use the same hookshot projectile
+				//TODO Respawn the fish at a random position
+				el.object3D.position.x = 4;
+				el.object3D.position.y = 2;
+				el.object3D.position.z = -3;
+				el.emit("updateVelocity", {x: -0.4, y: 0, z: 0}, false);
+			}
+		}
 	}
 });
 
@@ -96,12 +163,15 @@ AFRAME.registerComponent("velocity", {
 	init: function(){
 		var el = this.el;
 		var data = this.data;
-		//Assign variable to el so that it's editable from other components/entities
+		//Assign variables to el so that they're editable from other components/entities
 		el.vel = {x: data.vel.x, y: data.vel.y, z: data.vel.z};
-		this.gravity = data.gravity;
+		el.gravity = data.gravity;
 		
 		el.addEventListener("updateVelocity", function(e){
 			el.vel = {x: e.detail.x, y: e.detail.y, z: e.detail.z};
+		});
+		el.addEventListener("updateGravity", function(e){
+			el.gravity = e.detail.gravity;
 		});
 	},
 	
@@ -113,9 +183,10 @@ AFRAME.registerComponent("velocity", {
 		el.object3D.position.y += el.vel.y * dt;
 		el.object3D.position.z += el.vel.z * dt;
 		
-		el.vel.y += this.gravity * dt;
+		el.vel.y += el.gravity * dt;
 		if(el.vel.y < -10){ //Limit falling speed
 			el.vel.y = -10;
 		}
+		//TODO automatically retract hookshot if falling for too long or too low
 	}
 });
